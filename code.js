1328bc0556f80c7d04185476d3ca6fe7a8ead110
Alex Craig <158104649+acraig9266@users.noreply.github.com>
2024-12-05 16:00:22 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
 function quicksort(array) {
+    var high = array.length - 1;
+    var low = 0;
+
+
+    for (count = 0; count < array.length; count *= 2) {
+        sortHelp(array, low, high)
+        // Now figure out the new high and or pivots
+    }
+
     return array;
+}
+
+function sortHelp(arr, lo, hi) {
+    if (lo >= hi) {
+        return;
+    }
+    piv = lo;
+
+    for (i = lo + 1; i <= hi; i++) {
+        if (array[i] < array[lo]) {
+            swap(array[++piv], array[i]);
+        }
+    }
+    swap(arr[lo], arr[piv]);
+    return;
 }
