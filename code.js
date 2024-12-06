70f625a7c1268285f0179a7950a6e3e3e5bc1b8c
Alex Craig <158104649+acraig9266@users.noreply.github.com>
2024-12-05 17:02:07 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,20 @@
 function quicksort(array) {
-    var high = array.length - 1;
+    var high = array.length;
+    console.log(high);
     var low = 0;
 
-
-    for (count = 0; count < array.length; count *= 2) {
-        sortHelp(array, low, high)
-        // Now figure out the new high and or pivots
+    for (count = array.length; count > 1; count = Math.ceil(count / 2)) {
+        high = Math.ceil((array.length) / count);
+        newHigh = high;
+        low = 0; 
+        console.log(high);
+        for (sortNum = 1; sortNum <= array.length / count; sortNum++) {
+            console.log('Call Count: ', sortNum);
+            sortHelp(array, low, newHigh);
+            low = high + 1;
+            newHigh = high * sortNum;
+        }
     }
-
     return array;
 }
 
@@ -18,10 +25,18 @@ function sortHelp(arr, lo, hi) {
     piv = lo;
 
     for (i = lo + 1; i <= hi; i++) {
-        if (array[i] < array[lo]) {
-            swap(array[++piv], array[i]);
+        if (arr[i] < arr[lo]) {
+            swap(arr, ++piv, i);
         }
     }
-    swap(arr[lo], arr[piv]);
+    swap(arr, lo, piv);
     return;
 }
+
+function swap(arr, a, b) {
+    tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+}
+
+console.log(quicksort([2, 7, 6, 8, 4, 3]));
